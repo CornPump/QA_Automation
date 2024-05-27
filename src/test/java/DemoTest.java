e6a0b9f7052f047e22a01a6ceff557bf78fcc1a4
CornPump <nadavf1992@gmail.com>
2024-05-28 00:00:32 +0300

    Inception

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,16 @@
+/**
+ * The class containing your tests for the {@link Demo} class.  Make sure you
+ * test all methods in this class (including both 
+ * {@link Demo#main(String[])} and 
+ * {@link Demo#isTriangle(double, double, double)}).
+ */
+
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.PrintStream;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DemoTest {
+
+}
